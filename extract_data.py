ad9f54e25285b31705c2828ee6abaedbcbe36ca5
madewild <madewild@ulb.ac.be>
2020-01-20 21:02:07 +0100

    first pass

--- extract_data.py
+++ extract_data.py
@@ -5,6 +5,8 @@ import requests
 from bs4 import BeautifulSoup
 
 output = open("schools.tsv", "w")
+header = "name\taddress\n"
+output.write(header)
 
 secondary_url = "http://www.enseignement.be/index.php?page=25933&act=search&check=&unite=&geo_type=1&geo_prov=5&geo_cp=&geo_loca=&geo_mots=&reseau=111%2C126%2C123%2C122%2C121%2C131%2C132&opt_degre=&opt_tyen=&opt_domaine=0&opt_mots=&opt_groupe=11&opt_option="
 html = requests.get(secondary_url).text
--- get_coordinates.py
+++ get_coordinates.py
@@ -1,3 +1,23 @@
 """Retrieve geocoordinates with OSM Nominatim API"""
 
-url = f"https://nominatim.openstreetmap.org/search.php?format=json&q={address}"
+import pandas as pd
+import requests
+import json
+
+output = open("coordinates.tsv", "w")
+header = "name\tlat\tlong\n"
+output.write(header)
+
+df = pd.read_csv("schools.tsv", sep="\t")
+for i, school in enumerate(df["name"]):
+    address = df["address"][i]
+    url = f"https://nominatim.openstreetmap.org/search.php?format=json&q={address}"
+    r = requests.get(url)
+    json_code = json.loads(r.content)
+    if json_code:
+        lat = json_code[0]["lat"]
+        lon = json_code[0]["lon"]
+        output_string = f"{school}\t{lat}\t{lon}\n"
+        output.write(output_string)
+    else:
+        print(address)
