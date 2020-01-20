4ae4918646dab3f3c417911090084e427211fe70
madewild <madewild@ulb.ac.be>
2020-01-20 22:07:59 +0100

    remove osm code

--- get_coordinates.py
+++ get_coordinates.py
@@ -14,14 +14,11 @@ output.write(header)
 df = pd.read_csv("data/schools.tsv", sep="\t")
 for i, school in enumerate(df["name"]):
     address = df["address"][i]
-    #url = f"https://nominatim.openstreetmap.org/search.php?format=json&q={address}"
     url = f"https://maps.googleapis.com/maps/api/geocode/json?address={school}, {address}&key={api_key}"
     r = requests.get(url)
     json_code = json.loads(r.content)["results"]
     if json_code:
-        #lat = json_code[0]["lat"]
         lat = json_code[0]["geometry"]["location"]["lat"]
-        #lon = json_code[0]["lon"]
         lon = json_code[0]["geometry"]["location"]["lng"]
         output_string = f"{school}\t{lat}\t{lon}\n"
         output.write(output_string)
