ef2902c3e5586a3b832fd6165c99b724d77397c2
madewild <madewild@ulb.ac.be>
2020-01-20 14:57:57 +0100

    first draft

---
+++ plot_coordinates.py
@@ -0,0 +1,8 @@
+"""Plotting geocoordinates on a map with GeoPandas"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import descartes
+import geopandas as gpd
+from shapely.geometry import Point, Polygon
+
